070f1153efcd5f35ecd7e062d1cd8d3871ab7c2f
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-12-06 15:39:55 +0000

    :art: Update MainLayout component
    ヘッダ付きのボディ領域がスクロールするように修正する。

--- frontend/src/components/Layouts/MainLayout.tsx
+++ frontend/src/components/Layouts/MainLayout.tsx
@@ -1,5 +1,7 @@
 import { Box } from "@mui/material";
 
+import { ElHeader } from "../Elements";
+
 type Props = {
   children: React.ReactNode;
 };
@@ -7,7 +9,12 @@ type Props = {
 export const MainLayout = (props: Props) => {
   return (
     <Box height="100vh" minHeight="100vh">
-      {props.children}
+      <Box height={64}>
+        <ElHeader title="Toggl Dash" position="static" />
+      </Box>
+      <Box height="calc(100vh - 64px)" overflow="auto">
+        <Box p={(theme) => theme.spacing(2, 0)}>{props.children}</Box>
+      </Box>
     </Box>
   );
 };

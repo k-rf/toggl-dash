d8526fd888eb6bb1073178806130bddc91c96575
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-12-06 15:41:32 +0000

    :art: Update Home page component
    合わせてテーマも修正する。

--- frontend/src/features/home/page/index.tsx
+++ frontend/src/features/home/page/index.tsx
@@ -1,41 +1,60 @@
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Stack } from "@mui/material";
+import { useState } from "react";
+
+import { ElIconButton } from "~/components/Elements/ElIconButton";
+import { UnfoldLessIcon, UnfoldMoreIcon } from "~/components/Icons";
 
 import { DashButton } from "../components/DashButton/DashButton";
 
 export const HomePage = () => {
+  const [expandedList, setExpandedList] = useState([false, false, false, false, false, false]);
+
+  const handleChange = (index: number) => {
+    setExpandedList((current) => [
+      ...current.slice(0, index),
+      !current.at(index),
+      ...current.slice(index + 1),
+    ]);
+  };
+
+  const handleShrinkAll = () => {
+    setExpandedList((current) => current.map(() => false));
+  };
+
+  const handleExpandAll = () => {
+    setExpandedList((current) => current.map(() => true));
+  };
+
   return (
-    <Container maxWidth="lg" sx={{ height: "inherit", display: "flex", alignItems: "center" }}>
-      <Box width="100%">
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <DashButton
-              details="details"
-              expanded
-              onChange={() => {}}
-              onStart={() => {}}
-              summary="summary"
-            />
+    <Container maxWidth="lg" sx={{ height: "inherit" }}>
+      <Stack spacing={2}>
+        <Box>
+          {expandedList.every(Boolean) ? (
+            <ElIconButton onClick={handleShrinkAll}>
+              <UnfoldLessIcon />
+            </ElIconButton>
+          ) : (
+            <ElIconButton onClick={handleExpandAll}>
+              <UnfoldMoreIcon />
+            </ElIconButton>
+          )}
+        </Box>
+        <Box width="100%" sx={{ display: "flex", alignItems: "top" }}>
+          <Grid container spacing={2}>
+            {expandedList.map((expanded, i) => (
+              <Grid item lg={3} md={4} sm={6} xs={12} key={`dash-button-${i}`}>
+                <DashButton
+                  details="details"
+                  expanded={expanded}
+                  onChange={() => handleChange(i)}
+                  onStart={() => {}}
+                  summary="summary"
+                />
+              </Grid>
+            ))}
           </Grid>
-          <Grid item xs={4}>
-            <DashButton
-              details="details"
-              expanded
-              onChange={() => {}}
-              onStart={() => {}}
-              summary="summary"
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <DashButton
-              details="details"
-              expanded
-              onChange={() => {}}
-              onStart={() => {}}
-              summary="summary"
-            />
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </Stack>
     </Container>
   );
 };
--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -1,6 +1,12 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
+  palette: {
+    background: {
+      default: "#A4CCF4",
+      paper: "#D7EBFE",
+    },
+  },
   components: {
     MuiButton: {
       styleOverrides: {

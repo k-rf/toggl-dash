65e9f9e86e967225d38d15c1e43df25afef6c1f1
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-12-31 15:40:45 +0000

    :package: Add cross-fetch
    スナップショットテストを取得しようとしたときにエラーが発生した。
    そのときのエラーメッセージに書かれた提案に従い cross-fetch を追加する。

--- frontend/src/lib/apollo-client.ts
+++ frontend/src/lib/apollo-client.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import fetch from "cross-fetch";
 
 const httpLink = new HttpLink({
   uri: "/api",
+  fetch,
 });
 
 export const apolloClient = new ApolloClient({ link: httpLink, cache: new InMemoryCache() });

c510c87f56b1b3d70c14d9d4e6f187701822e132
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-01-06 18:06:52 +0000

    :sparkles: Add a new feature
    指定した Toggl Client に紐づく Toggl Project の一覧を取得できる。

--- backend/src/features/dash-button/controller/get-all-toggl-client.resolver.ts
+++ backend/src/features/dash-button/controller/get-all-toggl-client.resolver.ts
@@ -1,13 +1,23 @@
 import { Query, Resolver } from "@nestjs/graphql";
 
 import { GetAllTogglClientService } from "../core/service/get-all-toggl-client/get-all-toggl-client.service";
+import { GetTogglProjectByClientService } from "../core/service/get-toggl-project-by-client/getl-toggl-project-by-client.service";
 
 @Resolver("TogglClient")
 export class TogglClientResolver {
-  constructor(private readonly service: GetAllTogglClientService) {}
+  constructor(
+    private readonly clientService: GetAllTogglClientService,
+    private readonly projectService: GetTogglProjectByClientService
+  ) {}
 
   @Query()
   async togglClientAll() {
-    return this.service.handle();
+    return this.clientService.handle();
   }
+
+  // NOTE: 429 レスポンスが返ってくるため見送り
+  // @ResolveField()
+  // async projects(@Parent() client: TogglClient) {
+  //   return this.projectService.handle(client.id);
+  // }
 }

aab29abcbdd1501f233a072b0d7cd28dfe460438
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-03-28 15:46:44 +0000

    📷 #2 Update snapshots

---
+++ frontend/src/components/Elements/ElIconButton.spec.tsx
@@ -0,0 +1,31 @@
+import { composeStories } from "@storybook/testing-react";
+import { act, render } from "@testing-library/react";
+
+import { AppProvider } from "~/provider";
+import { sleep } from "~/utils/sleep";
+
+import * as stories from "./ElIconButton.stories";
+
+const composedStories = composeStories(stories);
+
+describe("ElIconButton", () => {
+  const stories = Object.values(composedStories).map((Story) => {
+    return { storyName: Story.storyName, Story };
+  });
+
+  it.each(stories)("$storyName", async ({ Story }) => {
+    const { baseElement } = render(<Story />, {
+      wrapper: ({ children }) => {
+        return <AppProvider>{children}</AppProvider>;
+      },
+    });
+
+    act(() => {
+      sleep(10);
+    });
+
+    expect(baseElement).toMatchSnapshot();
+    await Story.play({ canvasElement: baseElement } as never);
+    expect(baseElement).toMatchSnapshot();
+  });
+});

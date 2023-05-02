de7d6752ffdd5067001bedd43d22913ff79662be
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-05-02 16:27:15 +0000

    ✨ #2 Add AnnualObjectivePrismaRepository

---
+++ backend/src/features/objective-achievement-tracker/infrastructure/annual-objective.prisma.repository.ts
@@ -0,0 +1,81 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { v4 as uuidV4 } from "uuid";
+
+import { PrismaService } from "~/config/database/prisma.service";
+import { Ok } from "~/util/result";
+
+import { AnnualObjectiveId, AnnualObjective } from "../core/domain/annual-objective";
+import { AnnualObjectiveRepository } from "../core/domain/annual-objective/annual-objective.repository";
+
+@Injectable()
+export class AnnualObjectivePrismaRepository implements AnnualObjectiveRepository {
+  private readonly logger = new Logger(AnnualObjectivePrismaRepository.name);
+
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findById(_id: AnnualObjectiveId): Promise<Result<AnnualObjective, undefined>> {
+    throw new Error("Not implemented");
+  }
+
+  async save(value: AnnualObjective): Promise<Result<AnnualObjective, undefined>> {
+    await this.prismaService.$transaction([
+      this.prismaService.annualObjective.create({
+        include: {
+          objective: true,
+        },
+        data: {
+          id: value.id.toPrimitive(),
+          year: value.year.toPrimitive(),
+          objective: {
+            createMany: {
+              data: value.objectives.toPrimitive().map((e) => {
+                const [hour, minute, second] = e.objectiveTime;
+                return { id: uuidV4(), clientId: e.clientId, hour, minute, second };
+              }),
+            },
+          },
+        },
+      }),
+      ...value.monthlyAvailableTimes
+        .toPrimitive()
+        .map((e) => {
+          const monthlyAvailableTimeId = uuidV4();
+
+          return [
+            this.prismaService.monthlyAvailableTime.create({
+              data: {
+                id: monthlyAvailableTimeId,
+                month: e.month,
+                annualObjectiveId: value.id.toPrimitive(),
+                off: e.offDay,
+              },
+            }),
+            this.prismaService.availableTime.create({
+              data: {
+                id: uuidV4(),
+                monthlyAvailableTimeId,
+                weight: e.weekday.weight,
+                ...this.toHms(e.weekday.time),
+              },
+            }),
+            this.prismaService.availableTime.create({
+              data: {
+                id: uuidV4(),
+                monthlyAvailableTimeId,
+                weight: e.holiday.weight,
+                ...this.toHms(e.holiday.time),
+              },
+            }),
+          ];
+        })
+        .flat(),
+    ]);
+
+    return Ok(value);
+  }
+
+  private toHms = (value: readonly [number, number, number]) => {
+    const [hour, minute, second] = value;
+    return { hour, minute, second };
+  };
+}
--- backend/src/features/objective-achievement-tracker/objective-achievement-tracker.module.ts
+++ backend/src/features/objective-achievement-tracker/objective-achievement-tracker.module.ts
@@ -3,13 +3,13 @@ import { Module } from "@nestjs/common";
 import { RegisterAnnualObjectiveResolver } from "./controller/register-annual-objective.resolver";
 import { AnnualObjectiveRepository } from "./core/domain/annual-objective/annual-objective.repository";
 import { RegisterAnnualObjectiveWorkflow } from "./core/service";
-import { AnnualObjectiveInMemoryRepository } from "./infrastructure/annual-objective.in-memory.repository";
+import { AnnualObjectivePrismaRepository } from "./infrastructure/annual-objective.prisma.repository";
 
 @Module({
   providers: [
     RegisterAnnualObjectiveResolver,
     RegisterAnnualObjectiveWorkflow,
-    { provide: AnnualObjectiveRepository, useClass: AnnualObjectiveInMemoryRepository },
+    { provide: AnnualObjectiveRepository, useClass: AnnualObjectivePrismaRepository },
   ],
 })
 export class ObjectiveAchievementTrackerModule {}
